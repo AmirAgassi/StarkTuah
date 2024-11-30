726bc27b65121700d163f64c55fdaa294578d858
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-30 00:03:44 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/navbar";
 import Landing from "./components/Landing";
 import Dashboard from "./components/Dashboard";
 import { useAccount } from "@starknet-react/core";
-import SwapInterface from "./components/SwapInterface";
 
 export default function App() {
   const { address, isConnected } = useAccount();
@@ -19,9 +18,6 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Navbar />
-      <main className="container mx-auto px-6 pt-20 mt-16">
-        <SwapInterface />
-      </main>
       {isConnected ? <Dashboard /> : <Landing />}
     </div>
   );
