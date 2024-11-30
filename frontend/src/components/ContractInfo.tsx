42a872e2ff049113a0da3086d7a9ade29847a9d3
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-30 13:50:35 -0500

    aaa

--- frontend/src/components/ContractInfo.tsx
+++ frontend/src/components/ContractInfo.tsx
@@ -2,7 +2,7 @@ import { useReadContract } from "@starknet-react/core";
 import { useState, useEffect } from "react";
 import type { Abi } from "starknet";
 export const CONTRACT_ADDRESS =
-  "0x075e0de21f64d08977275d528c8168fb03dc8904b8a34ffd4bbb18fbb94d844b";
+  "0x0071c918e88b3272cb79aa378b8ca607e25d11b4d259a86fe0d52489a2a8c4d6";
 
 export const USDC_ADDRESS =
   "0x07ab0b8855a61f480b4423c46c32fa7c553f0aac3531bbddaa282d86244f7a23";
