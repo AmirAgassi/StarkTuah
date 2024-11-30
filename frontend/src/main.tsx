c95f42ffdf9cc20f928f1e5df80440601c72ae5a
Xavier D'Mello <xaviermdmello@gmail.com>
2024-11-29 22:37:27 -0500

    connector button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { InjectedConnector } from "starknetkit/injected";
+import { ArgentMobileConnector } from "starknetkit/argentMobile";
+import { WebWalletConnector } from "starknetkit/webwallet";
+import { mainnet, sepolia } from "@starknet-react/chains";
+import { Connector, StarknetConfig, publicProvider } from "@starknet-react/core";
 
-createRoot(document.getElementById('root')!).render(
+const chains = [mainnet, sepolia];
+const connectors = [
+  new InjectedConnector({ options: { id: "braavos", name: "Braavos" } }),
+  new InjectedConnector({ options: { id: "argentX", name: "Argent X" } }),
+  new WebWalletConnector({ url: "https://web.argent.xyz" }),
+  new ArgentMobileConnector(),
+];
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StarknetConfig
+      chains={chains}
+      provider={publicProvider()}
+      connectors={connectors as Connector[]}
+    >
+      <App />
+    </StarknetConfig>
+  </StrictMode>
+);
